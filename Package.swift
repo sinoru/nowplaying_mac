70634fe483553f71e2333139bc074a45059cc226
Jaehong Kang <sinoru@me.com>
2016-12-05 22:45:03 +0900

    Using STwitter public one

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "NowPlaying-PM",
     dependencies: [
-        .Package(url: "https://github.com/sinoru/STwitter-Swift.git", "2.0.0-develop.20161205132659")
+        .Package(url: "https://github.com/sinoru/STwitter.git", "2.0.0-develop.20161205134347")
         ],
     exclude: ["NowPlaying"]
 )
